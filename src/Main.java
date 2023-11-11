5a80739f4f8c7f1d5ca0fc3678f02a314dba268b
Carevi4 <zenakukolnikov@rambler.ru>
2023-11-11 14:07:05 +0300

    newcode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(dev(14));
+        System.out.println("Результат: " + dev(14));
     }
 
     public static int dev(int n) {
